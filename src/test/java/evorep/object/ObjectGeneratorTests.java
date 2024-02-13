a57b8201551ae06a6a396f53f6ad66dce849eb52
Facundo Molina <facu.molina7@gmail.com>
2024-02-13 15:55:40 +0100

    Very simple tests for the object generation

---
+++ src/test/java/evorep/object/ObjectGeneratorTests.java
@@ -0,0 +1,18 @@
+package evorep.object;
+
+import org.junit.jupiter.api.Test;
+
+public class ObjectGeneratorTests {
+
+
+  @Test
+  public void testGenerateObjectsInitialization() {
+    // TODO: Implement test
+  }
+
+  @Test
+  public void testGenerateObjectsSortedList() {
+    ObjectGenerator.generateObjects("evorep.object.SortedListTestSuite");
+  }
+
+}
