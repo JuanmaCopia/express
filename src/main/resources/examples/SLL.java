a0b2931097f10d2e389369bb23949670451b3ecc
JuanmaCopia <jmcopia96@gmail.com>
2024-02-19 15:04:41 +0100

    minor change in example sll repok

--- src/main/resources/examples/SLL.java
+++ src/main/resources/examples/SLL.java
@@ -9,6 +9,25 @@ public class SLL {
     public Node head;
     public int size;
 
+    public boolean repOKStructure() {
+        if (head == null) {
+            return true;
+        }
+        Set<Node> visited = new HashSet<Node>();
+        LinkedList<Node> worklist = new LinkedList<Node>();
+        worklist.add(head);
+        while (!worklist.isEmpty()) {
+            Node current = worklist.removeFirst();
+            if (!visited.add(current)) {
+                return false;
+            }
+            if (current.next != null) {
+                worklist.add(current.next);
+            }
+        }
+        return true;
+    }
+
     private class Node {
         int data;
         Node next;
@@ -16,23 +35,6 @@ public class SLL {
         public Node(int data) {
             this.data = data;
         }
-    }
-
-    public boolean repOKStructure() {
-        Set<Node> visited = new HashSet<Node>();
-        LinkedList<Node> worklist = new LinkedList<Node>();
-        worklist.add(head);
-        while (!worklist.isEmpty()) {
-            Node current = worklist.removeFirst();
-            if (current == null) {
-                continue;
-            }
-            if (!visited.add(current)) {
-                return false;
-            }
-            worklist.add(current.next);
-        }
-        return true;
     }
 
 }
