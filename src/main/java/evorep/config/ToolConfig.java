804c946ec4b453adc36745433c62bbb8171c4cd9
Facundo Molina <facu.molina7@gmail.com>
2024-02-22 17:17:29 +0100

    Reading test source and test suite class name from ToolConfig

--- src/main/java/evorep/config/ToolConfig.java
+++ src/main/java/evorep/config/ToolConfig.java
@@ -10,8 +10,11 @@ public class ToolConfig {
 
     // Subject Settings
     public static String srcPath;
+    public static String testSrcPath;
     public static String binPath;
     public static String className;
+    public static String testSuiteClassName;
+
     public static int srcJavaVersion;
 
     // Search Settings
@@ -26,8 +29,10 @@ public class ToolConfig {
             properties.load(input);
 
             srcPath = properties.getProperty("evorep.src_path");
+            testSrcPath = properties.getProperty("evorep.test_src_path");
             binPath = properties.getProperty("evorep.bin_path");
             className = properties.getProperty("evorep.class_name");
+            testSuiteClassName = properties.getProperty("evorep.test_suite_class_name");
             srcJavaVersion = Integer.parseInt(properties.getProperty("evorep.src_java_version"));
 
             maxPopulation = Integer.parseInt(properties.getProperty("evorep.search.max_population"));
@@ -35,8 +40,10 @@ public class ToolConfig {
 
             System.out.println("\n -----------------  Configuration Parameters  -----------------\n");
             System.out.println("\tevorep.src_path = " + srcPath);
+            System.out.println("\tevorep.test_src_path = " + testSrcPath);
             System.out.println("\tevorep.bin_path = " + binPath);
             System.out.println("\tevorep.class_name = " + className);
+            System.out.println("\tevorep.test_suite_class_name = " + testSuiteClassName);
             System.out.println("\tevorep.src_java_version = " + srcJavaVersion);
             System.out.println("");
             System.out.println("\tevorep.search.max_population = " + maxPopulation);
