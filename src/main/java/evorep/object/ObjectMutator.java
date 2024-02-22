49a9b03c655e6498de8b1f0e2776fbc5b0f97bfb
Facundo Molina <facu.molina7@gmail.com>
2024-02-22 11:40:56 +0100

    Starting with ObjectMutator

---
+++ src/main/java/evorep/object/ObjectMutator.java
@@ -0,0 +1,18 @@
+package evorep.object;
+
+/**
+ * This class provides methods to perform random mutations on a given object
+ * @author Facundo Molina <facundo.molina@imdea.org>
+ */
+public class ObjectMutator {
+
+  /**
+   * Perform a random mutation on the given object
+   * @param object the object to mutate
+   * @return the mutated object
+   */
+  public static Object mutate(Object object) {
+    // TODO: Implement mutation
+    return null;
+  }
+}
