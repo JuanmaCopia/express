a22d833782b5cd246a3b22f9d9db59241ca6cecb
Facundo Molina <facu.molina7@gmail.com>
2024-02-22 11:40:05 +0100

    Adding Instrumenter class

---
+++ src/main/java/evorep/object/Instrumenter.java
@@ -0,0 +1,37 @@
+package evorep.object;
+
+import evorep.spoon.SpoonFactory;
+import evorep.spoon.SpoonQueries;
+import spoon.reflect.code.CtCodeSnippetStatement;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtVariable;
+import spoon.reflect.factory.CodeFactory;
+
+import java.util.List;
+
+/**
+ * This class allows to perform the instrumentation of the tests in the test suite
+ * @author Facundo Molina <facundo.molina@imdea.org>
+ */
+public class Instrumenter {
+
+  /**
+   * Instrument the given test method by adding a call to the ObjectCollector.collectObject method at the end of the method
+   * @param method the test method to instrument
+   */
+  public static void instrumentMethod(CtClass<?> clazz, CtMethod<?> method) {
+    System.out.println("Instrumenting method: " + method.getSimpleName());
+    CodeFactory codeFactory = SpoonFactory.getCodeFactory();
+    List<CtVariable<?>> localVariableList = SpoonQueries.getLocalVariables(method);
+    for (CtVariable<?> variable : localVariableList) {
+      if (variable.getType().getSimpleName().equals("SortedList")) {
+        CtCodeSnippetStatement statement = codeFactory.createCodeSnippetStatement("evorep.object.ObjectCollector.saveObject(" + variable.getSimpleName() + ")");
+        method.getBody().addStatement(statement);
+      }
+    }
+    System.out.println("Method now is:");
+    System.out.println(method);
+  }
+
+}
