138ab9ff6e7ebb88e4ee4c32062e330f06380d4b
Facundo Molina <facu.molina7@gmail.com>
2024-02-22 11:38:04 +0100

    ObjectCollector includes a method to preserve objects

--- src/main/java/evorep/object/ObjectCollector.java
+++ src/main/java/evorep/object/ObjectCollector.java
@@ -1,10 +1,18 @@
 package evorep.object;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * This class allows provides methods for instrumenting the test suite and generating objects from it.
+ * This class provides methods that are included in test methods in order to save the objects
  * @author Facundo Molina <facundo.molina@imdea.org>
  */
 public class ObjectCollector {
 
+  public static List<Object> positiveObjects = new ArrayList<>();
+
+  public static void saveObject(Object object) {
+    positiveObjects.add(object);
+  }
 
 }
