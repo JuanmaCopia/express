17a2379732e5a9c776b382f5bd46e13753f12e40
JuanmaCopia <jmcopia96@gmail.com>
2024-11-04 14:45:07 +0100

    added cases to run_case_study script

--- run_case_study.sh
+++ run_case_study.sh
@@ -7,11 +7,18 @@ CASE_STUDY=$1
 
 TREEMAP_PLI="casestudies/pli/treemap/treemap.properties"
 SCHEDULE_PLI="casestudies/pli/schedule/schedule.properties"
+SCHEDULE_ARRAY_PLI="casestudies/pli/schedulearray/schedulearray.properties"
 HASHMAP_PLI="casestudies/pli/hashmap/hashmap.properties"
 LINKEDLIST_PLI="casestudies/pli/linkedlist/linkedlist.properties"
 TREESET_PLI="casestudies/pli/treeset/treeset.properties"
 AVLTREE_PLI="casestudies/pli/avltree/avltree.properties"
 BINOMIALHEAP_PLI="casestudies/pli/binomialheap/binomialheap.properties"
+
+NODECACHINGLINKEDLIST_APACHE="casestudies/apache/nodecachinglinkedlist/nodecachinglinkedlist.properties"
+
+LINKEDLIST_JAVAUTIL="casestudies/javautil/linkedlist/linkedlist.properties"
+TREEMAP_JAVAUTIL="casestudies/javautil/treemap/treemap.properties"
+
 
 case $CASE_STUDY in
     "TREEMAP_PLI")
@@ -19,6 +26,9 @@ case $CASE_STUDY in
         ;;
     "SCHEDULE_PLI")
         ./gradlew run --args="$SCHEDULE_PLI"
+        ;;
+    "SCHEDULE_ARRAY_PLI")
+        ./gradlew run --args="$SCHEDULE_ARRAY_PLI"
         ;;
     "HASHMAP_PLI")
         ./gradlew run --args="$HASHMAP_PLI"
@@ -35,10 +45,17 @@ case $CASE_STUDY in
     "BINOMIALHEAP_PLI")
         ./gradlew run --args="$BINOMIALHEAP_PLI"
         ;;
-    "TRANSPORTSTATS_PLI")
-        ./gradlew run --args="$TRANSPORTSTATS_PLI"
+    "NODECACHINGLINKEDLIST_APACHE")
+        ./gradlew run --args="$NODECACHINGLINKEDLIST_APACHE"
+        ;;
+    "TREEMAP_JAVAUTIL")
+        ./gradlew run --args="$TREEMAP_JAVAUTIL"
+        ;;
+    "LINKEDLIST_JAVAUTIL")
+        ./gradlew run --args="$LINKEDLIST_JAVAUTIL"
         ;;
     *)
-        echo "Invalid CASE_STUDY value"
+        echo ""
+        echo "ERROR: Invalid CASE_STUDY argument"
         ;;
 esac
